03086236de1cbbf4ebd4f1e1b6fdca983939788a
JadeCheah <tszskygypei2522@gmail.com>
2025-01-23 20:36:03 +0800

    Level-2 Add, List done

--- src/main/java/Jorja.java
+++ src/main/java/Jorja.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Jorja {
     private static final String DIVIDER = "____________________________________________________________\n";
+    private static ArrayList<String> tasks;
 
     private static void greetUser() {
         String greeting = DIVIDER +
@@ -10,8 +13,9 @@ public class Jorja {
         System.out.println(greeting);
     }
 
-    public static void echo(String message) {
-        String echo = DIVIDER + message + "\n" + DIVIDER;
+    public static void addTask(String message) {
+        tasks.add(message);
+        String echo = DIVIDER + "added: " + message + "\n" + DIVIDER;
         System.out.println(echo);
     }
 
@@ -20,16 +24,28 @@ public class Jorja {
         System.out.println(exitMessage);
     }
 
+    public static void listTasks() {
+        System.out.println(DIVIDER);
+        for (int i = 0; i < tasks.size(); i++) {
+            String output = (i + 1) + ". " + tasks.get(i);
+            System.out.println(output);
+        }
+        System.out.println(DIVIDER);
+    }
+
     public static void main(String[] args) {
         greetUser();
+        tasks = new ArrayList<String>();
         Scanner scanner = new Scanner(System.in);
         while (true) {
              String input = scanner.nextLine();
              if (input.equalsIgnoreCase("bye")) {
                  exit();
                  break;
+             } else if (input.equalsIgnoreCase("list")) {
+                 listTasks();
              } else {
-                 echo(input);
+                 addTask(input);
              }
         }
         scanner.close();
