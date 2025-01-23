ba43f7a9fb144bc0d8a6cb1205c3b0c3dc62b348
JadeCheah <tszskygypei2522@gmail.com>
2025-01-23 16:35:36 +0800

    Level-0 greet user done

---
+++ src/main/java/Jorja.java
@@ -0,0 +1,15 @@
+public class Jorja {
+
+    private static void greetUser() {
+        String greeting = "____________________________________________________________\n" +
+                " Hello! I'm Jorja\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println(greeting);
+    }
+    public static void main(String[] args) {
+        greetUser();
+    }
+}
