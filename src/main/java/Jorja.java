2c160e1c5510725ddf2bb3ce2ddc73a793834ee1
JadeCheah <tszskygypei2522@gmail.com>
2025-01-23 17:10:27 +0800

    Level-1 echo user done

--- src/main/java/Jorja.java
+++ src/main/java/Jorja.java
@@ -1,15 +1,37 @@
+import java.util.Scanner;
 public class Jorja {
+    private static final String DIVIDER = "____________________________________________________________\n";
 
     private static void greetUser() {
-        String greeting = "____________________________________________________________\n" +
+        String greeting = DIVIDER +
                 " Hello! I'm Jorja\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
+                DIVIDER;
         System.out.println(greeting);
     }
+
+    public static void echo(String message) {
+        String echo = DIVIDER + message + "\n" + DIVIDER;
+        System.out.println(echo);
+    }
+
+    public static void exit() {
+        String exitMessage = DIVIDER + "Bye until we meet again!\n" + DIVIDER;
+        System.out.println(exitMessage);
+    }
+
     public static void main(String[] args) {
         greetUser();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+             String input = scanner.nextLine();
+             if (input.equalsIgnoreCase("bye")) {
+                 exit();
+                 break;
+             } else {
+                 echo(input);
+             }
+        }
+        scanner.close();
     }
 }
