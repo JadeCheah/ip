f75c5239c85931cfca8d525d82b8ed7d08fa25cd
JadeCheah <tszskygypei2522@gmail.com>
2025-02-21 22:46:19 +0800

    Modify JavaDoc for Launcher main method

--- src/main/java/bart/Launcher.java
+++ src/main/java/bart/Launcher.java
@@ -6,11 +6,6 @@ import javafx.application.Application;
  * A launcher class to workaround classpath issues.
  */
 public class Launcher {
-    /**
-     * The main entry point for launching the JavaFX application.
-     *
-     * @param args Command-line arguments passed to the application.
-     */
     public static void main(String[] args) {
         Application.launch(Main.class, args);
     }
